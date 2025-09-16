ddfbd11eecb19e0d2ff9473e5bddc00d0d3b0207
GabRann <146079174+GabRann@users.noreply.github.com>
2025-09-16 18:16:13 +0200

    Add files via upload

---
+++ guide/GuideProfileScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function GuideProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Guide Profile Screen</Text>
+    </View>
+  );
+}
